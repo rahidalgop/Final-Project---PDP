5b5449353137adfe6f7d2e0dc134280c2fca84fc
Raúl Hidalgo Pérez <160086484+rahidalgop@users.noreply.github.com>
2024-07-27 18:58:40 -0600

    Add files via upload

---
+++ globalVariables.py
@@ -0,0 +1,50 @@
+# Variables
+# ========================================================================================
+
+administrator = {
+    "id":"000000000",
+    "name":"Administrator",
+    "password":"000",
+    "profile":"administrator",
+    "gender":"female",
+    "age":20,
+    "residency":"cq",
+    "birthdate":"february"
+}
+
+isabel = {
+    "id":"604840803",
+    "name":"Isa",
+    "password":"123",
+    "profile":"citizen",
+    "gender":"female",
+    "age":20,
+    "residency":"cq",
+    "birthdate":"february"
+}
+
+angelica = {
+    "id":"208480382",
+    "name":"Ange",
+    "password":"456",
+    "profile":"police",
+    "gender":"female",
+    "age":"20",
+    "residency":"cq",
+    "birthdate":"november"
+}
+
+raul = {
+    "id":"207820653",
+    "name":"Miaul",
+    "password":"789",
+    "profile":"judge",
+    "gender":"male",
+    "age":25,
+    "residency":"cq",
+    "birthdate":"august"
+}
+
+users = [administrator, isabel, angelica, raul]
+
+currentUser = 0
---
+++ vehicleCollision.py
@@ -0,0 +1,12 @@
+
+# Libraries
+# ========================================================================================
+import loginFunctions
+from globalVariables import *
+
+# Main body
+# ========================================================================================
+
+if __name__ == "__main__":
+
+    loginFunctions.displayLoginMenu()
